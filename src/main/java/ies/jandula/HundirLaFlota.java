6ee0fcf5cda22d626c70104d3e0f2074ed9ba299
Manuel <manucarseg@gmail.com>
2025-01-29 17:44:36 +0100

    Primera parte del juego Hundir la flota, hemos integrado la colocación de barcos y copiado el tablero.

--- src/main/java/ies/jandula/HundirLaFlota.java
+++ src/main/java/ies/jandula/HundirLaFlota.java
@@ -9,14 +9,16 @@ public class HundirLaFlota {
         //Esto que he puesto aqui es una guia de como dividirnos el trabajo
         //Nada de esto es definitivo, puesto va a ir cambiando por si nos hace falta otros metodos
     
-        // Crear los tableros de ambos jugadores (3x3)
         Scanner teclado = new Scanner(System.in);
 
         //Declaracion de variables
 
         char[][] taleroJugador1 = {{'A', 'A', 'A'}, {'A', 'A', 'A'}, {'A', 'A', 'A'}};
         char[][] taleroJugador2 = {{'A', 'A', 'A'}, {'A', 'A', 'A'}, {'A', 'A', 'A'}};
+        char[][] taleroFinalJugador1 = new char[3][3];
+        char[][] taleroFinalJugador2 = new char[3][3];
 
+        //Introducimos un mensaje de bienvenida y una pequeña explicación del juego.
         System.out.println("¡Bienvenido jugadores al juego de hundir la flota!");
         System.out.println("Vamos a explicar brevemente las reglas del juego.");
         System.out.println("Cada jugador tendrá un tablero de 3x3 y dos barcos que colocará en secreto.");
@@ -30,6 +32,18 @@ public class HundirLaFlota {
         System.out.println("Si tocamos por completo el barco, nos dira tocado y hundido, con esto eliminamos el barco.");
         System.out.println("Gana el jugador que hunda todos los barcos del jugador rival.");
         System.out.println("Aclarado todo esto, ¡Empezemos la partida!");
+        System.out.println();
+        System.out.println();
+
+        //Le pedimos a los jugadores que nos introduzcan las posiciones de los barcos.
+        System.out.println("Jugador 1, por favor coloca tus barcos");
+        colocarBarcos(taleroJugador1);
+        System.out.println("Jugador 2, por favor coloca tus barcos");
+        colocarBarcos(taleroJugador2);
+
+        //Guardo en una nueva tabla la posición de los barcos, para luego pintarlo por pantalla.
+        copiarTablero(taleroJugador1, taleroFinalJugador1);
+        copiarTablero(taleroJugador2, taleroFinalJugador2);
 
         //esctructura más bien orientativa.
     }
@@ -37,4 +51,65 @@ public class HundirLaFlota {
     //Uno de nosotros (Alumno A) se dedicará a introducir los barcos, validar las posiciones, que puedan estar adyacentes y no se solapen.
     //El Alumno B se dedicara a las funciones de los disparos, de verificar si ha tocado, si ha dado agua o si ha tocado y hundido.
     //Y el Alumno A se dedicará a recoger ese resultado, finalizar el juego y pintar por pantalla las posiciones de los barcos delos jugadores en los tableros.
+    public static void colocarBarcos(char[][] tablero) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        //declaración de variables
+        int contadorBarcos = 1;
+        int fila1 = 0;
+        int col1 = 0;
+        int fila2 = 0;
+        int col2 = 0;
+
+        //Realizamos un bucle para introducir los barcos.
+        while (contadorBarcos <= 2) {
+            System.out.println("Introduce la posición del barco " + contadorBarcos + " (debes introducir las coordenadas por fila y columna)");
+            System.out.println("Introduce la fila 1: ");
+            fila1 = teclado.nextInt() - 1;
+            System.out.println("Introduce la columna 1: ");
+            col1 = teclado.nextInt() - 1;
+            System.out.println("Introduce la fila 2: ");
+            fila2 = teclado.nextInt() - 1;
+            System.out.println("Introduce la columna 2: ");
+            col2 = teclado.nextInt() - 1;
+
+            //Con este if me aseguro de que no meten un digito erroneo.
+            if (fila1 < 0 || fila1 >= 3 || col1 < 0 || col1 >= 3 || fila2 < 0 || fila2 >= 3 || col2 < 0 || col2 >= 3) {
+                System.out.println("Los parametros introducidos no son validos.");
+                continue;
+            }
+            //Con este if me aseguro de que no coloquen barcos en el mismo sitio.
+            if (tablero[fila1][col1] == 'B' || tablero[fila2][col2] == 'B') {
+                System.out.println("No puedes colocar un barco en el mismo lugar.");
+                continue;
+            }
+            //Con este if (me he tenido que ayudar de internet para sacar esto).
+            //Hago que el barco sea de dos unidades adyacentes.
+            //Reconozcon que sin internet, a lo mejor no lo sacaba, pero era lo que tenía en mente y queria hacer.
+            if ((fila1 == fila2 && Math.abs(col1 - col2) == 1) || (col1 == col2 && Math.abs(fila1 - fila2) == 1)) {
+                tablero[fila1][col1] = 'B';
+                tablero[fila2][col2] = 'B';
+                contadorBarcos++;
+            } else {
+                //Si meten algún segundo barco que solape con el primero o que las posiciones del barco no esten adyacentes, les saldrá este mensaje.
+                System.out.println("Posición inválida. Asegúrate de que el barco es de dos posiciones adyacentes y no se solapan.");
+            }
+
+        }
+    }
+    public static char[][] copiarTablero(char[][] tableroOriginal) {
+
+        //Esta función es básica, leemos el array del tablero de cualquier jugador y la copiamos en el nuevo tablero.
+        char[][] copia = new char[tableroOriginal.length][tableroOriginal[0].length];
+
+        for (int i = 0; i < tableroOriginal.length; i++) {
+            for (int j = 0; j < tableroOriginal[i].length; j++) {
+                copia[i][j] = tableroOriginal[i][j];
+            }
+        }
+        //Con esto nos gaurdamos el tablero original para cuando tengamos que pintar por pantalla el tablero
+        //El tablero del jugador lo hiremos modificando y será peor a la hora de pintarlo.
+        return copia;
+    }
 }
